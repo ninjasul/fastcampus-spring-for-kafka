1882aeaac1220229e22ac632d52629f7d7a6f023
박동엽 <dypark@onestorecorp.com>
2022-07-04 19:42:03 +0900

    [03-kafka-producer] 기본 전송 준비 완료

--- 03-kafka-producer/src/main/java/com/fastcampus/springkafka/Application.java
+++ 03-kafka-producer/src/main/java/com/fastcampus/springkafka/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -13,8 +14,9 @@ public class Application {
     }
 
     @Bean
-    public ApplicationRunner runner() {
+    public ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
         return args -> {
+            kafkaTemplate.send("03-producer", "Hello, 03-producer");
         };
     }
 }
