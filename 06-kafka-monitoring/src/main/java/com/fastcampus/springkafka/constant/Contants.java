7f101fc5617c31fbe8ca9434d5f7eed05853af0f
박동엽 <dypark@onestorecorp.com>
2022-07-17 13:48:31 +0900

    [06-kafka-monitoring] counter 추가

--- 06-kafka-monitoring/src/main/java/com/fastcampus/springkafka/constant/Contants.java
+++ 06-kafka-monitoring/src/main/java/com/fastcampus/springkafka/constant/Contants.java
@@ -2,5 +2,7 @@ package com.fastcampus.springkafka.constant;
 
 public class Contants {
     public static final String CLIP6_LISTENER = "clip6-listener";
+    public static final String CLIP6_LISTENER_COUNTER = "clip6-listener-counter";
+    public static final String CLIP6_LISTENER_COUNTER_TAG_NAME = "group-id";
     public static final String CLIP6_TOPIC = "clip6";
 }
--- 06-kafka-monitoring/src/main/java/com/fastcampus/springkafka/consumer/Clip6Consumer.java
+++ 06-kafka-monitoring/src/main/java/com/fastcampus/springkafka/consumer/Clip6Consumer.java
@@ -1,18 +1,26 @@
 package com.fastcampus.springkafka.consumer;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import static com.fastcampus.springkafka.constant.Contants.CLIP6_LISTENER;
-import static com.fastcampus.springkafka.constant.Contants.CLIP6_TOPIC;
+import static com.fastcampus.springkafka.constant.Contants.*;
 
 @Service
 @Slf4j
 public class Clip6Consumer {
+    private final Counter counter;
+
+    public Clip6Consumer(MeterRegistry meterRegistry) {
+        this.counter = meterRegistry.counter(CLIP6_LISTENER_COUNTER, CLIP6_LISTENER_COUNTER_TAG_NAME, CLIP6_LISTENER);
+    }
 
     @KafkaListener(id = CLIP6_LISTENER, topics = CLIP6_TOPIC)
     public void listen(String message) {
         log.info("message: {}", message);
+        counter.increment();
     }
 }
